5289bcdf7ee87e926169f6387bfd7b8f8868f5c3
kayquesanmartin <kayque.sanmartin65@gmail.com>
2024-11-14 00:26:47 -0300

    Lógica corrigida

--- services/cliente_service.py
+++ services/cliente_service.py
@@ -1,13 +1,10 @@
 from models.cliente import Cliente
 from config import get_session
 
-
 def cadastrar_cliente(cpf, nome, email):
     session = get_session()
-
     try:
         cliente_existente = session.query(Cliente).filter(Cliente.cpf == cpf).first()
-
         if cliente_existente:
             print(f"Cliente com CPF {cpf} já cadastrado.")
         else:
@@ -18,5 +15,18 @@ def cadastrar_cliente(cpf, nome, email):
     except Exception as e:
         session.rollback()
         print(f"Ocorreu um erro ao cadastrar o cliente: {e}")
+    finally:
+        session.close()
+
+def listar_clientes():
+    session = get_session()
+    try:
+        clientes = session.query(Cliente).all()
+        if clientes:
+            print("Clientes cadastrados:")
+            for cliente in clientes:
+                print(f"CPF: {cliente.cpf}, Nome: {cliente.nome}, Email: {cliente.email}")
+        else:
+            print("Nenhum cliente cadastrado.")
     finally:
         session.close()
